4396282f8f2050ad931d4faac3c75dac96bc5edc
ledinhhuy <ledinhhuy0105@gmail.com>
2023-03-25 13:20:09 +0700

    complete !!!

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,8 @@
 var arrNum = [8,6,-20,10,4,-4,-16,9,7]
 console.log(arrNum);
 
+
+
 document.querySelector('#btn_themGT').onclick = function () {
     var arrayIndex = +document.querySelector('#nhapGTMang').value
     arrNum.push(arrayIndex)
@@ -8,6 +10,7 @@ document.querySelector('#btn_themGT').onclick = function () {
     document.querySelector('#arrayNumber').innerHTML = 'Mảng arrNum: [' + arrNum + ']'
 }
 
+//Bài 1: Tổng các số dương trong mảng
 document.querySelector('#btn_TinhTongCacSoDuong').onclick = function () {
     var sumPositiveNumber = 0
     for (var i = 0; i <= arrNum.length; i++) {
@@ -18,6 +21,7 @@ document.querySelector('#btn_TinhTongCacSoDuong').onclick = function () {
     document.querySelector('#tinhTongSoDuong').innerHTML = 'Tổng các số dương trong mảng =' + sumPositiveNumber
 }
 
+//Bài 2: Đếm có bao nhiêu số dương trong mảng
 document.querySelector('#btn_demCacSoDuong').onclick = function () {
     var n = 0
     for (var i = 0; i <= arrNum.length; i++) {
@@ -28,6 +32,7 @@ document.querySelector('#btn_demCacSoDuong').onclick = function () {
     document.querySelector('#demSoDuong').innerHTML = 'Số lượng số nguyên dương trong mảng là:' + n
 }
 
+//Bài 3: Tìm số nhỏ nhất trong mảng
 document.querySelector('#btn_minArray').onclick = function () {
     var minArray = arrNum[0]
     for (var i = 1; i <= arrNum.length; i++) {
@@ -38,6 +43,7 @@ document.querySelector('#btn_minArray').onclick = function () {
     document.querySelector('#minArray').innerHTML = 'Giá trị nhỏ nhất trong mảng:' + minArray
 }
 
+//Bài 4: Tìm số dương nhỏ nhất trong mảng
 document.querySelector('#btn_minArrayPositiveNum').onclick = function () {
     var arrPositiveNum = []
     for (var index = 0; index <= arrNum.length; index++) {
@@ -54,6 +60,7 @@ document.querySelector('#btn_minArrayPositiveNum').onclick = function () {
     document.querySelector('#minArrayPositiveNum').innerHTML = 'Số dương nhỏ nhất trong mảng:' + minPositiveNum
 }
 
+//Bài 5: Tìm số chẵn cuối cùng trong mảng. Nếu mảng không có giá trị chẵn thì trả về -1
 document.querySelector('#btn_soChanCuoiCung').onclick = function () {
     var arrSoChan = []
     for (var i = 0; i <= arrNum.length; i++) {
@@ -69,6 +76,7 @@ document.querySelector('#btn_soChanCuoiCung').onclick = function () {
     }
 }
 
+//Bài 7: Sắp xếp mảng theo thứ tự tăng dần
 document.querySelector('#btn_tangDan').onclick = function () {
     for (var i = 0; i <= arrNum.length; i++) {
         for (var j = i + 1; j < arrNum.length; j++) {
@@ -83,6 +91,7 @@ document.querySelector('#btn_tangDan').onclick = function () {
     document.querySelector('#tangDan').innerHTML = 'Xắp xếp theo thứ tự tăng dần:' + arrNum
 }
 
+//Bài 6: Đổi chỗ 2 giá trị trong mảng theo vị trí (Cho nhập vào 2 vị trí muốn đổi chỗ giá trị)
 document.querySelector('#btn_doiGiaTri').onclick = function () {
     var change_1 = +document.querySelector('#giatriChange_1').value
     var change_2 = +document.querySelector('#giatriChange_2').value
@@ -97,6 +106,8 @@ document.querySelector('#btn_doiGiaTri').onclick = function () {
     }
 }
 
+
+//Bài 8: Tìm số nguyên tố đầu tiên trong mảng. Nếu mảng không có số nguyên tố thì trả về – 1
 document.querySelector('#btn_soNTDauTien').onclick = function () {
     for (var index = 0; index < arrNum.length; index++) {
         if (findPrimeNumber(arrNum[index]) == true) {
@@ -113,7 +124,11 @@ document.querySelector('#btn_soNTDauTien').onclick = function () {
     }
 }
 
-
+/**
+ * Hàm này được sử dụng để kiểm tra số nguyên tố trong mảng
+ * @param {*} so giá trị kiểm tra số nguyên tố
+ * @returns 
+ */
 function findPrimeNumber(so) {
     var primeNumber = true
     for (var i = 2; i < Math.abs(so); i++) {
@@ -127,4 +142,35 @@ function findPrimeNumber(so) {
         }
     }
     return primeNumber
+}
+
+//Bài 9: Nhập thêm 1 mảng số thực, tìm xem trong mảng có bao nhiêu số nguyên?
+document.querySelector('#btn_demSoNguyen').onclick = function () {
+    var demSoNguyen = 0
+    for (var i=0;i<arrNum.length;i++) {
+        if (arrNum[i] % 1 === 0 ) {
+            demSoNguyen ++
+        }
+    }
+    document.querySelector('#demSoNguyen').innerHTML = 'Số nguyên trong mảng là:' + demSoNguyen
+}
+
+//Bài 10: So sánh số lượng số dương và số lượng số âm xem số nào nhiều hơn
+document.querySelector('#btn_soLuong').onclick = function () {
+    var soDuong = 0
+    var soAm = 0
+    for (var i=0;i<arrNum.length;i++) {
+        if (arrNum[i]>0) {
+            soDuong++
+        } else {
+            soAm++
+        }
+    }
+    var htmlOuput = ''
+    if (soAm>soDuong) {
+        htmlOuput = 'Số âm nhiều hơn số dương'
+    } else {
+        htmlOuput = 'Số dương nhiều hơn số âm'
+    }
+    document.querySelector('#soLuong').innerHTML = htmlOuput
 }
